635d667655179e99f896ccbe808353a312fa7518
adept <chernickovvit@yandex.ru>
2022-05-28 15:30:04 +0300

    Redesign

--- src/main/java/com/experimental/blog/Controllers/MainController.java
+++ src/main/java/com/experimental/blog/Controllers/MainController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
-    @GetMapping("/")
-    public String home(Model model) {
-        return "home";
-    }
-
     @GetMapping("/about")
     public String about(Model model) {
         return "about";
